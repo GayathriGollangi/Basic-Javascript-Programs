aa13f401bfc1beeb3e0af0ef93bfde78f30b9bfa
gayathrigollangi <gayathrigollangii@gmail.com>
2024-07-06 11:17:35 +0530

    Write a program to print greatest among the three numbers

---
+++ demo3.js
@@ -0,0 +1,10 @@
+let a = 15;
+let b = 9;
+let c = 8;
+if (a > b && a > c) {
+  console.log(a, "is greater");
+} else if (b > a && b > c) {
+  console.log(b, "is greater");
+} else {
+  console.log(c, "is greater");
+}
